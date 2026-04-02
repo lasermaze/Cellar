6667fda8d5bdea21bcb4136d94c274fd8c639f19
Peter <sook40@gmail.com>
2026-04-01 23:30:49 -0700

    feat(23-02): register InstallAppCommand in CellarCLI subcommands
    - Added InstallAppCommand.self after RemoveCommand.self (utility placement)
    - cellar install-app now appears in cellar --help

--- Sources/cellar/Cellar.swift
+++ Sources/cellar/Cellar.swift
@@ -5,7 +5,7 @@ struct Cellar: ParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "cellar",
         abstract: "Wine game launcher for old PC games on macOS",
-        subcommands: [StatusCommand.self, AddCommand.self, LaunchCommand.self, LogCommand.self, ServeCommand.self, SyncCommand.self, RemoveCommand.self],
+        subcommands: [StatusCommand.self, AddCommand.self, LaunchCommand.self, LogCommand.self, ServeCommand.self, SyncCommand.self, RemoveCommand.self, InstallAppCommand.self],
         defaultSubcommand: StatusCommand.self
     )
 
